574c791dc406422ae17eace0a445eae3561b50a1
Amit <rahimdhillon686@gmail.com>
2022-10-13 19:13:23 +0530

    rabbit work on requests

--- index.js
+++ index.js
@@ -41,22 +41,22 @@ function doconnect() {
 //         console.error(ex);
 //     }
 // }
-async function do_consume() {
-    var conn = amqplib.connect(amqp_url, "heartbeat=60");
-    var ch = await conn.createChannel()
-    var q = 'test_queue';
-    await conn.createChannel();
-    await ch.assertQueue(q, { durable: true });
-    await ch.consume(q, function (msg) {
-        console.log(msg.content.toString());
-        ch.ack(msg);
-        ch.cancel('myconsumer');
-    }, { consumerTag: 'myconsumer' });
-    setTimeout(function () {
-        ch.close();
-        conn.close();
-    }, 500);
-}
+// async function do_consume() {
+//     var conn = amqplib.connect(amqp_url, "heartbeat=60");
+//     var ch = await conn.createChannel()
+//     var q = 'test_queue';
+//     await conn.createChannel();
+//     await ch.assertQueue(q, { durable: true });
+//     await ch.consume(q, function (msg) {
+//         console.log(msg.content.toString());
+//         ch.ack(msg);
+//         ch.cancel('myconsumer');
+//     }, { consumerTag: 'myconsumer' });
+//     setTimeout(function () {
+//         ch.close();
+//         conn.close();
+//     }, 500);
+// }
 
 const app = express()
 app.all('/', (req, res) => {
